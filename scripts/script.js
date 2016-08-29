ea6070f5dd7d83b00e44526036b8bd1cfcfe36e3
Sam Lee <sam.lee.dtb@gmail.com>
2016-08-29 11:48:50 -0700

    interests scripts added

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function() { 
+
+/*-----------------------------------------------------------------*/
+	$('.i-d-large').hover(function() {
+
+		$(this).animate({
+	        height: "335px",
+	        width: "235px",
+    	},300);
+
+		$('.interests-text', this).fadeTo(400, 1)
+
+	}, function(){
+		$(this).animate({
+	        height: "325px",
+	        width: "225px"
+    	},200, function(){ 
+
+    	});
+
+		$('.interests-text', this).fadeTo(600, 0)
+
+	})
+
+	$('.i-d-small').hover(function() {
+
+		$(this).animate({
+	        height: "310px",
+	        width: "235px",
+    	},300);
+
+		$('.interests-text', this).fadeTo(400, 1)
+
+	}, function(){
+		$(this).animate({
+	        height: "300px",
+	        width: "225px"
+    	},200, function(){ 
+
+    	});
+
+		$('.interests-text', this).fadeTo(600, 0)
+
+	})
+
+});
+
